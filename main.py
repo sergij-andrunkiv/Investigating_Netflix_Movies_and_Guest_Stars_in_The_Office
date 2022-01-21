9778319dff787606c1d42da331acd8cb04ca0f12
Sergij Andrunkiv <sergij.andrunkiv@gmail.com>
2022-01-21 19:56:01 +0200

    created a lists and movie_dict

---
+++ main.py
@@ -0,0 +1,8 @@
+years = [2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020]
+durations = [103, 101, 99, 100, 100, 95, 95, 96, 93, 90]
+
+# A dictionary with the two lists
+movie_dict = {
+    'years': years,
+    'durations': durations
+}
