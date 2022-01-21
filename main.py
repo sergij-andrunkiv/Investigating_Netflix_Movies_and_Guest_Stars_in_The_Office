60e86404bc63415eb7f1582fe2e36e20a79a1151
Sergij Andrunkiv <sergij.andrunkiv@gmail.com>
2022-01-21 20:13:16 +0200

    added alias to pandas

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pandas
+import pandas as pd
 
 years = [2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020]
 durations = [103, 101, 99, 100, 100, 95, 95, 96, 93, 90]
@@ -9,4 +9,4 @@ movie_dict = {
     'durations': durations
 }
 
-durations_df = pandas.DataFrame(movie_dict)
+durations_df = pd.DataFrame(movie_dict)
