b291066a8a6539c04cc1f930c76f69d043f19d19
Sergij Andrunkiv <sergij.andrunkiv@gmail.com>
2022-01-21 20:16:11 +0200

    imported pandas, created and printed DataFrame

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import pandas
+
 years = [2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020]
 durations = [103, 101, 99, 100, 100, 95, 95, 96, 93, 90]
 
@@ -6,5 +8,5 @@ movie_dict = {
     'years': years,
     'durations': durations
 }
-
-print(movie_dict)
+durations_df = pandas.DataFrame(movie_dict)
+print(durations_df)
